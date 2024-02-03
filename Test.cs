431484e0340b07cd3ca676b502022263f8ea20da
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-02-03 21:24:17 +0000

    Initial commit

---
+++ Test.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Test {
+    public static int Main(string[] args) {
+        Console.WriteLine("Hello, world!");
+        
+        return 0;
+    }
+}
---
+++ build.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+cd "${0%/*}"
+find ./*/ -name "*.cs" -exec csc -debug Test.cs {} +
