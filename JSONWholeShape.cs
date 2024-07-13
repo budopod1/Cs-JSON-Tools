fe0c2cec83f77fcceafcd938a45ea8e7e77235d9
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-07-13 23:01:29 +0000

    Fixed: JSONWholeShape error message is now consistent with functionality

--- JSONWholeShape.cs
+++ JSONWholeShape.cs
@@ -4,10 +4,10 @@ public class JSONWholeShape : IJSONShape {
     public void Verify(IJSONValue value) {
         JSONInt intHolder = value as JSONInt;
         if (intHolder == null) {
-            throw new InvalidJSONException("Expected int", value);
+            throw new InvalidJSONException("Expected an integer", value);
         }
         if (intHolder.Value < 0) {
-            throw new InvalidJSONException("Expected value greater than 0", value);
+            throw new InvalidJSONException("Expected value greater than or equal to 0", value);
         }
     }
 }
